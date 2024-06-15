90cfd36a2c20aca0c95fe3537edd1680a1209d06
Mansi <mansisharma209aps@gmail.com>
2024-06-16 02:06:45 +0530

    Basic Setup

---
+++ chat/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.room_name
+    
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) # On deleting the room the messaged shld also get deleted
+    sender = models.CharField(max_length=50)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{str(self.room)} - {self.sender}" 
